12625aa79dfd8c327bd74c23884a0578582d2e71
tahmidxp96 <tahmidxp96@gmail.com>
2025-12-30 00:29:07 +0600

    Remove legacy React project files

--- components/Loader.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const Loader: React.FC = () => (
-    <div className="flex justify-center items-center py-20">
-        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-green-500"></div>
-    </div>
-);
